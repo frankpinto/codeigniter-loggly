e77dfa3561473820a15c45566c8ac4df7b3823bb
Frank Pinto <frank.joseph.pinto@gmail.com>
2013-05-23 16:10:14 -0400

    If no bugs, finished

--- Loggly.php
+++ Loggly.php
@@ -1,10 +1,31 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
 
+/* Loggly logging */
+require_once(APPPATH."lib/Pimple.php");
+require_once(APPPATH."lib/HasInputInterface.php");
+require_once(APPPATH."lib/LogglyInterface.php");
+require_once(APPPATH."lib/Input/InputInterface.php");
+require_once(APPPATH."lib/Input/HttpInputInterface.php");
+require_once(APPPATH."lib/Input/HttpInput.php");
+require_once(APPPATH."lib/Http/ClientInterface.php");
+require_once(APPPATH."lib/Http/AbstractClient.php");
+require_once(APPPATH."lib/Http/AsyncClient.php");
+require_once(APPPATH."lib/Http/BufferedAsyncClient.php");
+require_once(APPPATH."lib/ApiLogger.php");
+
 class Loggly {
+  $this->logger = null;
+  $this->CI = null;
 
-    public function __construct()
-    {
-    }
+  public function __construct()
+  {
+    $this->CI =& get_instance();
+    $loggly_key = $this->CI->config('loggly_key');
+    $this->logger = new Cowlby\Loggly\ApiLogger($loggly_key);
+  }
+
+  public function send($msg)
+  {
+    $this->logger->send($msg);
+  }
 }
-
-/* End of file Someclass.php */
